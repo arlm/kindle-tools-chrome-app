d0ac36ab131ba870c261ce0cdf3ebfe1f7bebf10
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@keyidentity.com>
2018-05-09 11:29:25 +0200

    Checking for USB support

--- ng-app/src/app/app.component.ts
+++ ng-app/src/app/app.component.ts
@@ -13,12 +13,33 @@ export class AppComponent {
   connected = false;
   processed = false;
   buttonText = 'Connect to Kindle device';
+  usb_enabled = false;
   @ViewChild(ClippingsComponent) clippings: ClippingsComponent;
 
   constructor(public snackBar: MatSnackBar) {
-    navigator.usb.getDevices({filters: [{ vendorId: 0x1949 }]}).then(devices => {
-      if (devices.length === 1) {
-        const device = devices[0];
+    this.usb_enabled = 'usb' in navigator;
+
+    if (this.usb_enabled) {
+      navigator.usb.getDevices({ filters: [{ vendorId: 0x1949 }] }).then(devices => {
+        if (devices.length === 1) {
+          const device = devices[0];
+          if (device.productId === 0x0002 || device.productId === 0x0004) {
+            this.connected = true;
+            this.buttonText = `Connected to the ${device.productName}`;
+            this.snackBar.open(`Connected to the "${device.productName}" device!`, 'OK', {
+              duration: 2000,
+            });
+          } else {
+            this.errorState();
+          }
+        }
+      });
+    }
+  }
+
+  connectKindle() {
+    if (this.usb_enabled) {
+      navigator.usb.requestDevice({ filters: [{ vendorId: 0x1949 }] }).then(device => {
         if (device.productId === 0x0002 || device.productId === 0x0004) {
           this.connected = true;
           this.buttonText = `Connected to the ${device.productName}`;
@@ -26,26 +47,12 @@ export class AppComponent {
             duration: 2000,
           });
         } else {
-          this.errorState();
+          this.errorState(device.productName);
         }
-      }
-    });
-  }
-
-  connectKindle() {
-    navigator.usb.requestDevice({filters: [{ vendorId: 0x1949 }]}).then(device => {
-        if (device.productId === 0x0002 || device.productId === 0x0004) {
-          this.connected = true;
-          this.buttonText = `Connected to the ${device.productName}`;
-            this.snackBar.open(`Connected to the "${device.productName}" device!`, 'OK', {
-              duration: 2000,
-            });
-      } else {
-        this.errorState(device.productName);
-      }
-    }, data => {
-      this.errorState();
-    });
+      }, data => {
+        this.errorState();
+      });
+    }
   }
 
   processClippingsFile(changeEvent) {
@@ -68,9 +75,9 @@ export class AppComponent {
 
 declare global {
   interface Navigator {
-      usb: {
-        requestDevice(filters: any): any;
-        getDevices(filters: any): any;
-      };
+    usb: {
+      requestDevice(filters: any): any;
+      getDevices(filters: any): any;
+    };
   }
 }
